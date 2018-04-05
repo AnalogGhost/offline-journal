d8f65c2f9d4cff7439cdf58076f19ac05bd4fee7
Mathew Brown <matt@mattbrown.com>
2018-04-05 13:36:16 -0600

    Add static file support Add no cache headers for offline files Add test files

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(express.static('public/online'))
+app.use(express.static('public/offline', {etag: false, setHeaders: noCacheHeaders}))
+
 app.get('/',(req,res) => res.send('Hello World'))
 
-
 app.listen(port, () => console.log(`Offline Journal listening on ${port}`))
+
+function noCacheHeaders(res, path, stat) {
+  console.log('set headers')
+  res.set({
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': 0
+  })
+}
